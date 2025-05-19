16669d8d6221a32440fd986fa1d0407037d85604
haard18 <haardsolanki.itm@gmail.com>
2025-05-19 13:17:51 +0530

    Refactor SmartWallet to use hashed PIN for authorization
    - Updated the transfer and changePin functions to utilize a hashed PIN instead of a plain PIN for enhanced security.
    - Renamed the transfer function parameter from `pin` to `pinHash` for clarity.
    - Removed the plain PIN storage and replaced it with a hashed version using keccak256.
    - Adjusted related modifiers and constructor to accommodate the new PIN hashing mechanism.

--- contracts/src/SmartWallet.sol
+++ contracts/src/SmartWallet.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 interface IEscrow {
     function deposit() external payable;
-    function transfer(address to, uint256 amount, uint256 pin) external;
+    function transfer(address to, uint256 amount, uint256 pinHash) external;
     function getBalance() external view returns (uint256);
 }
 
@@ -15,29 +15,29 @@ interface IEscrow {
 contract SmartWallet {
     IEscrow public immutable escrow;
     bool public paused;
-    uint256 private pin;
-    
+    bytes32 public pinHash;
+
     // Events
     event Paused();
     event Unpaused();
     event DepositReceived(address indexed from, uint256 amount);
     event TransferInitiated(address indexed to, uint256 amount);
     event PinChanged();
-    
+
     // TODO: Add identity component fields
     // bytes32 public uuid;
     // address public sbtContract;
-    
+
     modifier whenNotPaused() {
         require(!paused, "Contract is paused");
         _;
     }
-    
+
     modifier onlyWithPin(uint256 _pin) {
-        require(_pin == pin, "Invalid PIN");
+        require(keccak256(abi.encodePacked(_pin)) == pinHash, "Invalid PIN");
         _;
     }
-    
+
     /**
      * @dev Constructor sets up the escrow contract and initial PIN
      * @param _escrow Address of the escrow contract
@@ -47,9 +47,9 @@ contract SmartWallet {
         require(_escrow != address(0), "Invalid escrow address");
         require(_pin != 0, "Invalid PIN");
         escrow = IEscrow(_escrow);
-        pin = _pin;
+        pinHash = keccak256(abi.encodePacked(_pin));
     }
-    
+
     /**
      * @dev Receive function to handle direct ETH deposits
      */
@@ -57,7 +57,7 @@ contract SmartWallet {
         emit DepositReceived(msg.sender, msg.value);
         IEscrow(escrow).deposit{value: msg.value}();
     }
-    
+
     /**
      * @dev Fallback function to handle ETH deposits
      */
@@ -65,20 +65,24 @@ contract SmartWallet {
         emit DepositReceived(msg.sender, msg.value);
         IEscrow(escrow).deposit{value: msg.value}();
     }
-    
+
     /**
      * @dev Transfer funds to a recipient
      * @param to Recipient address
      * @param amount Amount to transfer
      * @param _pin PIN for authorization
      */
-    function transfer(address to, uint256 amount, uint256 _pin) external whenNotPaused onlyWithPin(_pin) {
+    function transfer(
+        address to,
+        uint256 amount,
+        uint256 _pin
+    ) external whenNotPaused onlyWithPin(_pin) {
         require(to != address(0), "Invalid recipient");
         require(amount > 0, "Amount must be greater than 0");
         emit TransferInitiated(to, amount);
         IEscrow(escrow).transfer(to, amount, _pin);
     }
-    
+
     /**
      * @dev Get the current balance in the escrow
      * @return Current balance
@@ -86,7 +90,7 @@ contract SmartWallet {
     function getBalance() external view returns (uint256) {
         return IEscrow(escrow).getBalance();
     }
-    
+
     /**
      * @dev Pause the contract
      * @param _pin PIN for authorization
@@ -95,7 +99,7 @@ contract SmartWallet {
         paused = true;
         emit Paused();
     }
-    
+
     /**
      * @dev Unpause the contract
      * @param _pin PIN for authorization
@@ -104,15 +108,18 @@ contract SmartWallet {
         paused = false;
         emit Unpaused();
     }
-    
+
     /**
      * @dev Change the PIN
      * @param oldPin Current PIN
      * @param newPin New PIN
      */
-    function changePin(uint256 oldPin, uint256 newPin) external onlyWithPin(oldPin) {
+    function changePin(
+        uint256 oldPin,
+        uint256 newPin
+    ) external onlyWithPin(oldPin) {
         require(newPin != 0, "Invalid new PIN");
-        pin = newPin;
+        pinHash = keccak256(abi.encodePacked(newPin));
         emit PinChanged();
     }
 }
