2706666bebe75c7b61fe6f6500a764ed8c8c4a8c
haard18 <haardsolanki.itm@gmail.com>
2025-05-24 14:38:40 +0530

    Add Button component for reusable button functionality

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = ({ children, onClick }:{ children: React.ReactNode, onClick: () => void }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="mt-2 bg-gradient-to-b from-[#D6F1FF] from-10% via-[#3B6CB7] via-60% to-[#1E3140] text-white border-none rounded-full py-3 font-semibold text-lg shadow-md w-full hover:opacity-90 transition"
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
